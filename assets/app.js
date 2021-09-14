a8aafcb2579b89abaa0e16d82e142fb950cdfd05
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-09-14 18:29:39 +0200

    Add file upload with ajax

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,37 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+jQuery(function(){
+    $("#imageField").on("change",function(){
+        var uploadForm = $("#imgUploadForm");
+        var formData = new FormData(uploadForm[0]);
+            $.ajax({
+                url: "/",
+                method: "post",
+                data: formData,
+                processData: false,
+                contentType: false,
+                async: true,
+                success: function(location)
+                {
+                    $("#imgIcon").remove();
+                    $("#img").attr("src",location);
+                },
+                error: function(){
+                    $("#resolutionError").addClass("show-msg");
+                    $("#resolutionError").removeClass("hide-msg");
+                    console.log("Img resolution is too high")
+                }
+                
+            })     
+    })
+})
+
+$("#hideMsgBtn").on("click",function(){
+    $("#resolutionError").addClass("hide-msg");
+    $("#resolutionError").removeClass("show-msg");
+})
+
+
--- src/Controller/PhotoEditorController.php
+++ src/Controller/PhotoEditorController.php
@@ -3,15 +3,33 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PhotoEditorController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('/index.html.twig', [
+        if($request->isXmlHttpRequest())
+        {
+            $imgFile = $request->files->get('uploadImg');
+            $imgSize = getimagesize($imgFile);
+            if(!is_null($imgFile) && $imgSize[0]<=1000 && $imgSize[1]<=1000){
+                $fileName = uniqid().".".$imgFile->getClientOriginalExtension();
+                $imgFile->move($this->getParameter("image_path"),$fileName);
+                $imgPath = "images/"."".$fileName;
+                $location = $imgPath;
+                return new JsonResponse($location);
+            }
+            else{
+                return new JsonResponse(null,500);
+            }   
+        }
+        
+        return $this->render('index.html.twig', [
             'controller_name' => 'PhotoEditorController',
         ]);
     }
