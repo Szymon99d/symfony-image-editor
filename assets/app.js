5fe8ed2a644ef48e85e48eab5126ea9d4cbbfebb
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-09-15 13:19:37 +0200

    Draw image onto the canvas and apply selected filters

--- assets/app.js
+++ assets/app.js
@@ -11,8 +11,12 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
+var canvas = document.getElementById('image');
+var ctx = canvas.getContext('2d');
+var img = new Image();
 
 jQuery(function(){
+    //if the file was selected, send a request to the server
     $("#imageField").on("change",function(){
         var uploadForm = $("#imgUploadForm");
         var formData = new FormData(uploadForm[0]);
@@ -23,15 +27,21 @@ jQuery(function(){
                 processData: false,
                 contentType: false,
                 async: true,
-                success: function(location)
+                success: function(imgPath)
                 {
                     $("#imgIcon").remove();
-                    $("#img").attr("src",location);
+                    img.src = imgPath;
+                    img.onload = function() {
+                        //set the canvas size to the image size
+                        canvas.width = img.width;
+                        canvas.height = img.height;
+                        ctx.drawImage(img,0,0);
+                    }
+                    
                 },
                 error: function(){
                     $("#resolutionError").addClass("show-msg");
                     $("#resolutionError").removeClass("hide-msg");
-                    console.log("Img resolution is too high")
                 }
                 
             })     
@@ -44,3 +54,28 @@ $("#hideMsgBtn").on("click",function(){
 })
 
 
+var filtersNames = ['contrast','sepia','grayscale','blur','brightness'];
+var filtersValues = [1,0,0,0,1];
+
+
+function setFilters()
+{
+    var filters="";
+    for(var i=0; i<filtersValues.length; i++)
+        filters+=filtersNames[i]+"("+filtersValues[i]+")"+" ";
+
+    ctx.filter = filters; //apply filters to canvas
+    ctx.drawImage(img,0,0); //draw an image on canvas with filters
+}
+
+
+$('input[type=range]').on('input', function () {
+    
+    var x = (element)=>element==this.id;
+    if(this.id=="blur")
+        filtersValues[filtersNames.findIndex(x)] = this.value+"px";
+    else
+        filtersValues[filtersNames.findIndex(x)] = this.value;
+    setFilters();
+
+});
