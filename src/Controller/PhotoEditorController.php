aa5b45d7cd593797023fe940fd030c17f060a664
Szymon99d <57864883+Szymon99d@users.noreply.github.com>
2021-09-15 12:53:29 +0200

    Add file extension check

--- src/Controller/PhotoEditorController.php
+++ src/Controller/PhotoEditorController.php
@@ -13,20 +13,37 @@ class PhotoEditorController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function index(Request $request): Response
     {
+        //check if the request is an ajax request
         if($request->isXmlHttpRequest())
         {
-            $imgFile = $request->files->get('uploadImg');
-            $imgSize = getimagesize($imgFile);
-            if(!is_null($imgFile) && $imgSize[0]<=1000 && $imgSize[1]<=1000){
-                $fileName = uniqid().".".$imgFile->getClientOriginalExtension();
-                $imgFile->move($this->getParameter("image_path"),$fileName);
-                $imgPath = "images/"."".$fileName;
-                $location = $imgPath;
-                return new JsonResponse($location);
+            $imgFile = $request->files->get('uploadImg'); //get selected image
+            $imgSize = getimagesize($imgFile); //get image width and height
+            $imgExt = $imgFile->getClientOriginalExtension(); //get image extension
+            //check image extension
+            if($imgExt=="png" || $imgExt=="jpg" || $imgExt=="jpeg")
+            {
+                if(!is_null($imgFile) && $imgSize[0]<=1000 && $imgSize[1]<=1000){
+                
+                    //set unique name and original extension
+                    $fileName = uniqid().".".$imgFile->getClientOriginalExtension();
+                    
+                    //move the file to the directory where you want to store it 
+                    $imgFile->move($this->getParameter("image_path"),$fileName);
+                    
+                    //send back image path 
+                    $imgPath = "images/"."".$fileName;
+
+                    return new JsonResponse($imgPath);
+                }
+                else{
+                    return new JsonResponse(null,406);
+                }  
             }
-            else{
-                return new JsonResponse(null,500);
-            }   
+            else
+            {
+                return new JsonResponse(null,406);
+            }
+ 
         }
         
         return $this->render('index.html.twig', [
